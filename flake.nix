18213254ffa4ae6b319b89db0bc124b2c5923d31
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-14 13:28:54 +0200

    refactor: restructure outputs in flake.nix for improved clarity and organization

--- flake.nix
+++ flake.nix
@@ -7,25 +7,32 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        # NixOS systems
-        nixosConfigurations = {
-          master = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./hosts/master.nix
-            ];
-          };
-
-          worker = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./hosts/worker.nix
-            ];
-          };
+    let
+      supportedSystems = flake-utils.lib.defaultSystems;
+      eachSystem = flake-utils.lib.eachDefaultSystem;
+    in {
+      nixosConfigurations = {
+        master = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/master.nix
+          ];
         };
-      });
+
+        worker = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/worker.nix
+          ];
+        };
+      };
+
+      # Optional: place for default outputs like packages/apps per system
+      packages = eachSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          # Future packages can go here
+        }
+      );
+    };
 }
