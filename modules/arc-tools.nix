c5b0178eae42809eff2da18b75d3b59d0757e08e
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-14 13:32:55 +0200

    refactor: update arc-tools.nix to use consistent namespace and improve path references

--- modules/arc-tools.nix
+++ modules/arc-tools.nix
@@ -1,49 +1,55 @@
 { pkgs, ... }:
 
-{
+let
+  controllerValues = ../../arc/controller/values.yaml;
+  runnerSetDir = ../../arc/runner-set;
+in {
   environment.systemPackages = with pkgs; [
+
+    # ARC controller install
     (pkgs.writeShellScriptBin "arc-deploy" ''
       helm upgrade --install arc \
         oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller \
-        --namespace custom-arc-systems \
+        --namespace arc-system \
         --create-namespace \
-        --values /etc/nixos/arc/controller/values.yaml
+        --values ${toString controllerValues}
     '')
 
+    # ARC controller uninstall
     (pkgs.writeShellScriptBin "arc-uninstall" ''
-      helm uninstall arc --namespace custom-arc-systems || true
+      helm uninstall arc --namespace arc-system || true
     '')
 
+    # Runner sets install
     (pkgs.writeShellScriptBin "arc-runners-deploy" ''
-      for file in /etc/nixos/arc/runner-set/*.yaml; do
+      for file in ${toString runnerSetDir}/*.yaml; do
         name=$(basename "$file" .yaml)
         echo "Deploying runner set: $name"
         helm upgrade --install "$name" \
           oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set \
-          --namespace custom-arc-runners \
+          --namespace arc-system \
           --create-namespace \
           --values "$file"
       done
     '')
 
+    # Runner sets upgrade
     (pkgs.writeShellScriptBin "arc-runners-upgrade" ''
       arc-runners-deploy
     '')
 
+    # Runner sets uninstall
     (pkgs.writeShellScriptBin "arc-runners-uninstall" ''
-      for file in /etc/nixos/arc/runner-set/*.yaml; do
+      for file in ${toString runnerSetDir}/*.yaml; do
         name=$(basename "$file" .yaml)
         echo "Uninstalling runner set: $name"
-        helm uninstall "$name" --namespace custom-arc-runners || true
+        helm uninstall "$name" --namespace arc-system || true
       done
     '')
 
+    # Status checker
     (pkgs.writeShellScriptBin "arc-status" ''
-      kubectl get pods -n custom-arc-runners
-    '')
-
-    (pkgs.writeShellScriptBin "arc-status-watch" ''
-      watch -n 2 kubectl get pods -n "custom-arc-runners"
+      kubectl get pods -n arc-system
     '')
   ];
 }
