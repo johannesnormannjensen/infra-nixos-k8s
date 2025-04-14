f162a3880127aead80bd73b95facf0e38810d355
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-14 10:55:25 +0200

    trye this

--- flake.nix
+++ flake.nix
@@ -3,30 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        nixosConfigurations = {
-          master = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./hosts/common.nix
-              ./hosts/master.nix
-            ];
-          };
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      master = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/common.nix
+          ./hosts/master.nix
+        ];
+      };
 
-          worker1 = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./hosts/common.nix
-              ./hosts/worker1.nix
-            ];
-          };
-        };
-      });
+      worker1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/common.nix
+          ./hosts/worker1.nix
+        ];
+      };
+    };
+  };
 }
