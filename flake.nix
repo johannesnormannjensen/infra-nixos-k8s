8d60a6b07c87a80500e4ddb678e5cf71ec0914b0
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-14 11:11:00 +0200

    fix master

--- flake.nix
+++ flake.nix
@@ -7,21 +7,13 @@
 
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
-      master = nixpkgs.lib.nixosSystem {
+        master = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./hosts/common.nix
-          ./hosts/master.nix
+            ./hosts/common.nix
+            ./hosts/master.nix
         ];
-      };
-
-      worker1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/common.nix
-          ./hosts/worker1.nix
-        ];
-      };
+        };
     };
   };
 }
--- hosts/master.nix
+++ hosts/master.nix
@@ -1,3 +1,17 @@
 { config, pkgs, ... }: {
-  networking.hostName = "master";
+
+  networking.hostName = "k8s-master";
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    clusterInit = true;
+    extraServerArgs = "--write-kubeconfig-mode=644";
+  };
+
+  environment.systemPackages = with pkgs; [
+    k3s
+    kubectl
+    helm
+  ];
 }
