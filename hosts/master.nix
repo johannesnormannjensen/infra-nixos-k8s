c4e03c6cf52d390ffd94fc8e44f138b7b0c307c5
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-14 13:01:34 +0200

    add: create values.yaml for ARC controller Helm release configuration

--- hosts/master.nix
+++ hosts/master.nix
@@ -14,8 +14,20 @@
     extraFlags = "--write-kubeconfig-mode=644";
   };
 
+  # ARC controller Helm release
+  services.kubernetes.helmReleases.arc-controller = {
+    chart = "gha-runner-scale-set-controller";
+    repo = "oci://ghcr.io/actions/actions-runner-controller-charts";
+    version = "0.11.0";
+    namespace = "custom-arc-systems";
+    createNamespace = true;
+    valuesFiles = [ ./arc/controller/values.yaml ];
+  };
+
   environment.systemPackages = with pkgs; [
-    k3s kubectl helm
+    k3s
+    kubectl
+    helm
   ];
 
   boot.loader.systemd-boot.enable = true;
