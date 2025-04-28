64325bffa923a1d2d24e2de0b44e0f55043e8541
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-28 08:35:16 +0200

    wait for k3s

--- modules/ingress-controller.nix
+++ modules/ingress-controller.nix
@@ -28,15 +28,29 @@ in
     ];
 
     system.activationScripts.ingressController.text = ''
+      set -euo pipefail
+
+      echo "[+] Waiting for Kubernetes API to become ready..."
+      for i in {1..60}; do
+        if ${pkgs.kubectl}/bin/kubectl version --short; then
+          echo "[+] Kubernetes API is ready!"
+          break
+        fi
+        echo "Waiting for k3s API... ($i)"
+        sleep 2
+      done
+
       echo "[+] Installing nginx ingress controller"
 
       ${pkgs.kubectl}/bin/kubectl create namespace ingress-nginx --dry-run=client -o yaml | ${pkgs.kubectl}/bin/kubectl apply -f -
 
-      ${pkgs.helm}/bin/helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+      ${pkgs.helm}/bin/helm repo add ingress-nginx https://kubernetes.github.io/helm-charts
       ${pkgs.helm}/bin/helm repo update
 
       ${pkgs.helm}/bin/helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
         --namespace ingress-nginx
+
+      echo "[+] Done setting up ingress!"
     '';
   };
 }
