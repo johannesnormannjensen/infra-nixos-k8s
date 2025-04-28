cd378f476b67c6041192808df1e2ba63768bb1d9
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-28 08:55:15 +0200

    define script

--- modules/ingress-controller.nix
+++ modules/ingress-controller.nix
@@ -4,6 +4,39 @@ with lib;
 
 let
   cfg = config.ingressController;
+
+  # define the script to set up the ingress controller
+  setupIngressControllerScript = pkgs.writeShellScriptBin "setup-ingress-controller" ''
+    set -euo pipefail
+
+    echo "[+] Waiting for Kubernetes API to become ready..."
+    for i in {1..60}; do
+      if ${pkgs.kubectl}/bin/kubectl get nodes &> /dev/null; then
+        echo "[+] Kubernetes API is ready!"
+        break
+      fi
+      echo "Waiting for k3s API... ($i)"
+      sleep 2
+    done
+
+    if ${pkgs.kubectl}/bin/kubectl get deployment ingress-nginx-controller -n ingress-nginx &> /dev/null; then
+      echo "[+] Ingress controller already installed. Skipping setup."
+      exit 0
+    fi
+
+    echo "[+] Installing nginx ingress controller"
+
+    ${pkgs.kubectl}/bin/kubectl create namespace ingress-nginx --dry-run=client -o yaml | ${pkgs.kubectl}/bin/kubectl apply -f -
+
+    ${pkgs.helm}/bin/helm repo add ingress-nginx https://kubernetes.github.io/helm-charts
+    ${pkgs.helm}/bin/helm repo update
+
+    ${pkgs.helm}/bin/helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+      --namespace ingress-nginx
+
+    echo "[+] Done setting up ingress!"
+  '';
+
 in
 {
   options.ingressController = {
@@ -30,36 +63,7 @@ in
     environment.systemPackages = with pkgs; [
       kubectl
       helm
-      (pkgs.writeShellScriptBin "setup-ingress-controller" ''
-        set -euo pipefail
-
-        echo "[+] Waiting for Kubernetes API to become ready..."
-        for i in {1..60}; do
-          if ${pkgs.kubectl}/bin/kubectl get nodes &> /dev/null; then
-            echo "[+] Kubernetes API is ready!"
-            break
-          fi
-          echo "Waiting for k3s API... ($i)"
-          sleep 2
-        done
-
-        if ${pkgs.kubectl}/bin/kubectl get deployment ingress-nginx-controller -n ingress-nginx &> /dev/null; then
-          echo "[+] Ingress controller already installed. Skipping setup."
-          exit 0
-        fi
-
-        echo "[+] Installing nginx ingress controller"
-
-        ${pkgs.kubectl}/bin/kubectl create namespace ingress-nginx --dry-run=client -o yaml | ${pkgs.kubectl}/bin/kubectl apply -f -
-
-        ${pkgs.helm}/bin/helm repo add ingress-nginx https://kubernetes.github.io/helm-charts
-        ${pkgs.helm}/bin/helm repo update
-
-        ${pkgs.helm}/bin/helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
-          --namespace ingress-nginx
-
-        echo "[+] Done setting up ingress!"
-      '')
+      setupIngressControllerScript
     ];
 
     systemd.services.setup-ingress-controller-timer-service = lib.mkIf cfg.autoSetup {
@@ -68,12 +72,9 @@ in
       wants = [ "k3s.service" ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = pkgs.writeShellScript "run-setup-ingress-controller" ''
-          setup-ingress-controller
-        '';
+        ExecStart = "${setupIngressControllerScript}/bin/setup-ingress-controller";
       };
     };
-
 
     systemd.timers.setup-ingress-controller-timer = lib.mkIf cfg.autoSetup {
       description = "Timer to run ingress controller setup after boot";
